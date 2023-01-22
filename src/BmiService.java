d5a59e9dc387340bbeecb1d445406f2abed04fa0
Lepehin Nikita <Lepekhinnikita@gmail.com>
2023-01-22 18:32:48 +0400

    changes code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,7 @@
 public class BmiService {
     public double calculate(double weight, double height) {
-        double index = (weight / (height * height));
-        return index;
+        return (weight / (height * height));
     }
 
-    }
+}
 
